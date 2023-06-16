790ba7d51b70dd055fdb0069f6b320d5a4c6418a
HasanC14 <chowdhuryh999@gmail.com>
2023-06-16 11:30:02 +0600

    task view added

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+
+  const addTask = () => {
+    if (task.trim() !== "") {
+      setTaskList([...taskList, task]);
+      setTask("");
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTaskList = [...taskList];
+    updatedTaskList.splice(index, 1);
+    setTaskList(updatedTaskList);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Todo List</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter a task"
+        value={task}
+        onChangeText={(text) => setTask(text)}
+      />
+      <TouchableOpacity style={styles.addButton} onPress={addTask}>
+        <Text style={styles.addButtonText}>Add Task</Text>
+      </TouchableOpacity>
+      <FlatList
+        data={taskList}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
+          <View style={styles.taskItem}>
+            <Text style={styles.taskText}>{item}</Text>
+            <TouchableOpacity onPress={() => deleteTask(index)}>
+              <Text style={styles.deleteButtonText}>Delete</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -14,7 +57,44 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    padding: 16,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 4,
+    padding: 8,
+    marginBottom: 8,
+  },
+  addButton: {
+    backgroundColor: "#007bff",
+    padding: 12,
+    borderRadius: 4,
     alignItems: "center",
-    justifyContent: "center",
+    marginBottom: 16,
+  },
+  addButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  taskItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 8,
+  },
+  taskText: {
+    flex: 1,
+    fontSize: 16,
+  },
+  deleteButtonText: {
+    color: "#ff0000",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
